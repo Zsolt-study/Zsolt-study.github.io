1c70c2ae0236bac5f7ab4688a427d6fcf653e40a
Zsolt <cindel.zsolt@gmail.com>
2020-08-08 12:05:33 +0200

    Function calcAmuont has been added.

--- js/main.js
+++ js/main.js
@@ -1,1 +1,11 @@
 console.log("Helló Zsolt! Érezd jól magad!");
+
+function calcAmount() {
+    let price = 1200;
+    let extra = parseInt(document.querySelector("[name=extra]:checked").value);
+    let souce = parseInt(document.querySelector("[name=souce]:checked").value);
+    let quantity = parseInt(document.querySelector("[name=quantity]").value);
+    let amount =(price + extra + souce) * quantity;
+    let showAmount = document.querySelector("span.show-amount");
+    showAmount.innerHTML = amount
+}
